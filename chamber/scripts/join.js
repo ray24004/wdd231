ab700973dbc69a6fc2a9246d510f2a01cfb63b9b
ray24004 <wraymundo@byupathway.edu>
2025-02-05 01:43:39 -0300

    Add programatic membership level options, cards and modals

---
+++ chamber/scripts/join.js
@@ -0,0 +1,48 @@
+const membershipLevels = [
+    "Non-profit",
+    "Bronze",
+    "Silver",
+    "Gold"
+];
+
+const membershipSelect = document.querySelector("#membershipSelect");
+const membershipSection = document.querySelector("#membershipSection");
+const membershipDialogs = document.querySelector("#membershipDialogs");
+
+membershipLevels.forEach(level => {
+    const option = document.createElement("option");
+    option.innerText = level;
+    option.value = level.toLowerCase().replace("-", "");
+    membershipSelect.append(option);
+
+    const article = document.createElement("article");
+    article.innerHTML = `
+        <h3>${level}</h3>
+        <button>Learn more...</a>
+    `;
+    membershipSection.append(article);
+
+    const dialog = document.createElement("dialog");
+    dialog.innerHTML = `
+        <button>❌</button>
+        <h2>${level}</h2>
+        <p>
+            Benefits: <br>
+            - A <br>
+            - B <br>
+            - C <br><br>
+
+            Cost: $ 00.00
+        </p>
+    `;
+    membershipDialogs.append(dialog);
+
+    article.querySelector("button").addEventListener("click", e => {
+        dialog.showModal();
+    });
+
+    dialog.querySelector("button").addEventListener("click", e => {
+        dialog.close();
+    });
+});
+
