2899e2717c38625d1f94deb7d4625171be16da33
ray24004 <wraymundo@byupathway.edu>
2025-02-05 03:06:11 -0300

    Validation fixes, fix date in join results

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,11 +1,13 @@
 const resultsDiv = document.querySelector("#results");
 
 const params = new URLSearchParams(window.location.search);
+const timestamp = Number(params.get("timestamp"));
+const date = new Date(timestamp);
 
 resultsDiv.innerHTML = `
     <p><strong>Name:</strong> <br>${params.get("firstName")} ${params.get("lastName")}</p>
     <p><strong>Email:</strong> <br>${params.get("email")}</p>
     <p><strong>Mobile:</strong> <br>${params.get("mobile")}</p>
     <p><strong>Business:</strong> <br>${params.get("organization")}</p>
-    <p><strong>Timestamp:</strong> <br>${params.get("timestamp")}</p>
+    <p><strong>Date:</strong> <br>${date}</p>
 `;
